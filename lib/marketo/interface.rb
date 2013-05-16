eb1319de7b55e0aa3adebe218c766446f2a4b83e
vijayaraj <vijayaraj@freshdesk.com>
2013-05-16 13:26:41 +0530

    update in sync lead

--- lib/marketo/interface.rb
+++ lib/marketo/interface.rb
@@ -43,7 +43,7 @@ module Marketo
       return Lead.from_hash(response[:success_get_lead][:result][:lead_record_list][:lead_record])
     end
 
-    def sync_lead(cookie, user_args = {})
+    def sync_lead(email, cookie, user_args = {})
       raise Exception, "Email must be provided" if email.nil?
 
       if(cookie.nil? || (cookie.include?("token:") == false))
@@ -52,7 +52,7 @@ module Marketo
         @cookie = cookie.slice!(cookie.index("token:")..-1)
       end
 
-      lead = ParamsSyncLead.new(cookie, user_args)
+      lead = ParamsSyncLead.new(email, user_args)
       response = send_request("ns1:paramsSyncLead", {:return_lead => true, :lead_record => lead.to_hash, :marketo_cookie => @cookie})
       return Lead.from_hash(response[:success_sync_lead][:result][:lead_record])
     end
